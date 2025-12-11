621b6021ac6a5111cf3e8be6402fa8a8ef757329
Nikhil Trivedi <78478320+nikstrvd@users.noreply.github.com>
2025-12-11 18:47:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
 export default defineConfig({
-  base: "/nikhilportfolie/",
-})
+  plugins: [react()],
+  base: "/portfolie/",
+});
